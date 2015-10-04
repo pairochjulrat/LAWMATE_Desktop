572e0fc4d8e75b3dd9ece8e71956a45af964121c
pairochjulrat <pairochjulrat@gmail.com>
2015-10-05 01:16:16 +0700

    Move DB scripts folder.

--- src/com/lawyermate/Main.java
+++ src/com/lawyermate/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ppl_fx;
+package com.lawyermate;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -17,7 +17,7 @@ import javafx.stage.Stage;
  *
  * @author pairochjulrat
  */
-public class PPL_FX extends Application {
+public class Main extends Application {
     
     @Override
     public void start(Stage primaryStage) {
